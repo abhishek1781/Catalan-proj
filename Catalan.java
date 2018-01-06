84c201293f01ba8c214aa0da88471cc59fa39bb0
Abhishek Lamba <35113791+abhishek1781@users.noreply.github.com>
2018-01-07 01:50:38 +0530

    Add changes

---
+++ Catalan.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+import java.math.*;
+
+class Catalan {
+  
+  public static void main(String[]args ) 
+{
+   
+     Scanner input=new Scanner(System.in);
+  
+      int n=input.nextInt();
+   
+     BigInteger fact=BigInteger.valueOf(1);
+  
+      for(int i=1;i<=2*n;i++)
+
+        fact=fact.multiply(BigInteger.valueOf(i));
+   
+     BigInteger w=BigInteger.valueOf(1);
+  
+      for(int i=1;i<=n+1;i++)
+  
+      w=w.multiply(BigInteger.valueOf(i));
+ 
+       BigInteger k=BigInteger.valueOf(1);
+ 
+       for(int i=1;i<=n;i++)
+  
+      k=k.multiply(BigInteger.valueOf(i));
+  
+      BigInteger c,d;
+ 
+       c=w.multiply(k);
+  
+      d=fact.divide(c);
+
+        System.out.print(d);
+
+        
+    }
+
+}
